aea7979f94084f9451989c73d4ca93fdf3935d04
erick <ericktomas691@gmail.com>
2025-04-20 23:05:04 -0600

    nuevo

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <windows.h>
 #include <conio.h>
 
+
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define DERECHA 77
+#define ABAJO 80
+
 using namespace std;
 
 void gotoxy(int x, int y) {
@@ -14,8 +20,21 @@ void gotoxy(int x, int y) {
 
     SetConsoleCursorPosition(hCon, dwPos);
 }
-int main() {
 
+
+void OcultarCursor() {
+    HANDLE hCon;
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cci;
+    cci.dwSize = 5;
+    cci.bVisible = FALSE;
+
+    SetConsoleCursorInfo(hCon,&cci);
+}
+
+
+int main() {
+    OcultarCursor();
     int x = 10, y = 10;
     gotoxy(10, 10);
     cout << "*";
@@ -27,10 +46,10 @@ int main() {
             char tecla = _getch();
             gotoxy(x, y);
             cout << " ";
-            if (tecla == 'j') x--;
-            if (tecla == 'l') x++;
-            if (tecla == 'i') y--;
-            if (tecla == 'k') y++;
+            if (tecla == IZQUIERDA) x--;
+            if (tecla == DERECHA) x++;
+            if (tecla == ARRIBA) y--;
+            if (tecla == ABAJO) y++;
             gotoxy(x, y);
             cout << "*";
         }
